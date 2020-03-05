d33de9f36e9c93d4f9d4f4e4428b46be78c0a1d5
mitchellhamilton <mitchell@hamil.town>
2020-03-05 22:20:54 +1100

    Implement output-csv

--- packages/output-csv/src/index.ts
+++ packages/output-csv/src/index.ts
@@ -1,3 +1,14 @@
-export const output = {
-  // TODO: implement output
+import stringifyToCsv from "csv-stringify/lib/sync";
+import { Output } from "@markings/types";
+
+export const output: Output = {
+  getFile(markings) {
+    return stringifyToCsv(
+      markings.map(({ location, ...x }) => ({
+        ...x,
+        filename: location.filename
+      })),
+      { header: true }
+    );
+  }
 };
