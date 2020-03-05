e93814d3b7f66cc76bfcbc62cdeeaedbfb60990d
mitchellhamilton <mitchell@hamil.town>
2020-03-05 15:17:50 +1100

    Add next

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-typescript"],
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    "@babel/preset-react"
+  ],
   plugins: ["@babel/plugin-transform-runtime"]
 };
